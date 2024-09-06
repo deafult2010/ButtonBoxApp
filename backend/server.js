1747265b0421307cce5f464d07ea81a39175d5c2
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2024-09-06 22:23:43 +0100

    committing back end progress

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,34 @@
 const express = require("express");
-const connectDB = require("./config/db");
+// const connectDB = require("./config/db");
 const dotenv = require("dotenv");
-const userRoutes = require("./routes/userRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-const messageRoutes = require("./routes/messageRoutes");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+// const userRoutes = require("./routes/userRoutes");
+// const chatRoutes = require("./routes/chatRoutes");
+// const messageRoutes = require("./routes/messageRoutes");
+// const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
+const { chats } = require("./data/data");
 
 dotenv.config();
-connectDB();
+// connectDB();
 const app = express();
 
 app.use(express.json()); // to accept json data
 
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
+app.get("/", (req, res) => {
+    res.send("API Running!");
+});
 
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
-app.use("/api/message", messageRoutes);
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id)
+    res.send(singleChat);
+});
+
+// app.use("/api/user", userRoutes);
+// app.use("/api/chat", chatRoutes);
+// app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
@@ -40,14 +49,14 @@ if (process.env.NODE_ENV === "production") {
 // --------------------------deployment------------------------------
 
 // Error Handling middlewares
-app.use(notFound);
-app.use(errorHandler);
+// app.use(notFound);
+// app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
 const server = app.listen(
     PORT,
-    console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+    console.log(`Server running on PORT ${PORT}...`)
 );
 
 const io = require("socket.io")(server, {
