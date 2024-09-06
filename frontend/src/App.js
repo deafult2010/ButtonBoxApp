5d001e8ea76b7fa45e5403a15cd97e5402e783fb
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2024-09-06 22:22:11 +0100

    made more progress on front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import { Route } from "react-router-dom";
 import './App.css';
+import Chatpage from "./Pages/Chatpage";
+import Homepage from "./Pages/Homepage";
 
 function App() {
   return (
-    <div className="App">Hello</div>
+    <div className="App">
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={Chatpage} />
+    </div>
   );
 }
 
